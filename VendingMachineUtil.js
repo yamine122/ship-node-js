825b5fe8a947848b3e733769540818206bd58931
yamine122 <wjdtmd92@gmail.com>
2019-12-12 18:36:43 +0900

    191212

--- VendingMachineUtil.js
+++ VendingMachineUtil.js
@@ -7,12 +7,7 @@
 // reduce
 // filter
 const util = {
-	forTest(arr){},
-	forEachTest(arr){},
-	
-}
-function Looping() {
-	this.forTest = function (arr) {
+	forTest(arr){
 		let returnVal = []
 		for (let i = 0; i < arr.length; i++) {
 			if (arr[i] % 2 == 0) {
@@ -20,65 +15,59 @@ function Looping() {
 			}
 		}
 		return returnVal
-	}
-	this.forEachTest = function (arr) {
+	},
+	forEachTest(arr){
 		let returnVal = []
 		arr.forEach(i => { if (i % 2 == 0) returnVal.push(i) })
-		return returnVal
-	}
-	this.mapTest = function (arr) {
+		return returnVal		
+	},
+	mapTest(arr){
 		return arr.map(i => i * 100)
-	}
-	this.findTest = function (arr) {
+	},
+	findTest(arr){
 		return (arr.find(function (i) {
 			return i > 5
 		}))
-	}
-	this.findTest2 = function (arr, id) {
-		let resultVal = (arr.find(i => i.id == id))
-		return (resultVal == undefined) ? '찾는 id가 없습니다' : resultVal.name
-
-
-	}
-	this.reduceTest = function (arr) {
+	},
+	reduceTest(arr){
 		return arr.reduce((i, j) => i + j)
-	}
-	this.filterTest = function (arr) {
+	},
+	filterTest(arr){
 		return arr.filter(i => (i % 2 == 0))
-	}
-	this.ofTest = function (arr) {
+	},
+	ofTest(arr){
 		let temp = []
 		for (const item of arr) {
 			temp.push(item.name)
 		}
 		return temp
-	}
-	this.fromTest = function (arr) {
+	},
+	fromTest(arr){
 		return Array.from(arr, i => i + i)
-	}
-	this.argumentsTest = function () {
+	},
+	argumentsTest(){
 		return [...arguments]
-	}
-	this.joinTest = function () {
+	},
+	joinTest(){
 		return [...arguments].join("\n")
-	}
-	this.sortTest = function () {
+	},
+	sortTest(){
 		let t1 = [...arguments].sort()
 		let t2 = [...t1].reverse()
 		return [...t1, ...t2]
-	}
-	this.reverseTest = function () {
+	},
+	reverseTest(){
 		return [...[...arguments].sort().reverse()]
-	}
-	this.todos = function (arr) {
+	},
+	todos(){
 		const todos = arr
 
 		// 비교 함수
 		function compare(key) {
 			return function (a, b) {
 				// 프로퍼티 값이 문자열인 경우, - 산술 연산으로 비교하면 NaN이 나오므로 비교 연산을 사용한다.
-				return a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0);
-			};
+				return a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0)
+			}
 		}
 
 		// id를 기준으로 정렬
@@ -94,7 +83,8 @@ function Looping() {
 		}
 		return temp
 	}
-
+	
 }
+
 
 
